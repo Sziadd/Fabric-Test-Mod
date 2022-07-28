7cd7e024572b11be92e0671480cea8fac71121ef
Sziad08 <adamrszigeti@gmail.com>
2022-07-27 19:26:05 -0600

    Added: A Die Item

--- src/main/java/net/sziad/testmod/block/ModBlocks.java
+++ src/main/java/net/sziad/testmod/block/ModBlocks.java
@@ -4,17 +4,29 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
+import net.minecraft.block.OreBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.minecraft.util.registry.Registry;
 import net.sziad.testmod.TestMod;
+import net.sziad.testmod.item.ModItemGroup;
 
-public class ModBlocks {
+public class  ModBlocks {
 
     public static final Block AQUAMARINE_BLOCK = registerBlock("aquamarine_block",
-            new Block(FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()), ItemGroup.MISC);
+            new Block(FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()), ModItemGroup.AQUAMARINE);
+
+    public static final Block AQUAMARINE_ORE = registerBlock("aquamarine_ore",
+            new OreBlock(FabricBlockSettings.of(Material.STONE).strength(6f).requiresTool(),
+                    UniformIntProvider.create(3, 7)), ModItemGroup.AQUAMARINE);
+
+    public static final Block DEEPSLATE_AQUAMARINE_ORE = registerBlock("deepslate_aquamarine_ore",
+            new OreBlock(FabricBlockSettings.of(Material.STONE).strength(6f).requiresTool(),
+                    UniformIntProvider.create(3, 7)), ModItemGroup.AQUAMARINE);
+
 
     private static Block registerBlock(String name, Block block, ItemGroup group) {
         registerBlockItem(name, block, group);
--- src/main/java/net/sziad/testmod/item/ModItems.java
+++ src/main/java/net/sziad/testmod/item/ModItems.java
@@ -7,16 +7,23 @@ import net.minecraft.util.Identifier;
 import net.sziad.testmod.TestMod;
 
 import net.minecraft.util.registry.Registry;
+import net.sziad.testmod.item.custom.DiceItem;
 
 public class ModItems {
     public static final Item AQUAMARINE_INGOT = registerItem("aquamarine_ingot",
-            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+            new Item(new FabricItemSettings().group(ModItemGroup.AQUAMARINE)));
 
     public static final Item AQUAMARINE_NUGGET = registerItem("aquamarine_nugget",
-            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+            new DiceItem(new FabricItemSettings().group(ModItemGroup.AQUAMARINE).maxCount(1)));
 
     public static final Item RAW_AQUAMARINE = registerItem("raw_aquamarine",
-            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+            new Item(new FabricItemSettings().group(ModItemGroup.AQUAMARINE)));
+
+    public static final Item DIE = registerItem("die",
+            new DiceItem(new FabricItemSettings().group(ModItemGroup.AQUAMARINE).maxCount(1)));
+
+    public static final Item die_base = registerItem("die_base",
+            new Item(new FabricItemSettings().group(ModItemGroup.AQUAMARINE)));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(TestMod.MOD_ID, name), item);
